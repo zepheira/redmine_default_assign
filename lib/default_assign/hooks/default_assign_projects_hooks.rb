9b318df6f27d3901287cc3ae5e53ac9d25bca5e7
Syntactic Vexation <syntacticvexation@gmail.com>
2011-12-26 02:23:45 +1000

    changed project hook so that project-wide default assignee is the selected option in the dropdown, otherwise system-wide default assignee is

--- lib/default_assign/hooks/default_assign_projects_hooks.rb
+++ lib/default_assign/hooks/default_assign_projects_hooks.rb
@@ -4,9 +4,11 @@ class DefaultAssignProjectHook < Redmine::Hook::ViewListener
   # * :form - the form builder
   # * :project - the current project
   def view_projects_form(context={})
- 
-    return content_tag(:p,
-		context[:form].select(:default_assignee_id,  context[:project].assignable_users.collect {|user| [user.name, user.id]}, {:include_blank => :none}))
+    selected = context[:project].default_assignee.id unless context[:project].default_assignee.blank?
+    selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? # FIX also make sure system-wide default is an assignable user
+    selected ||= []
+    return content_tag(:p, context[:form].select(:default_assignee_id,  
+                                      options_from_collection_for_select(context[:project].assignable_users, :id, :name, Integer(selected)), {:include_blank => :none}))
  
   end
 end
