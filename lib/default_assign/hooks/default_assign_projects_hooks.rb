9a3845d859501f819b4f9761a89add483e88a5fc
Syntactic Vexation <syntacticvexation@gmail.com>
2011-12-26 22:49:24 +1000

    closes #174 - display project default assign now respects whether global default is an assignable user

--- lib/default_assign/hooks/default_assign_projects_hooks.rb
+++ lib/default_assign/hooks/default_assign_projects_hooks.rb
@@ -5,8 +5,9 @@ class DefaultAssignProjectHook < Redmine::Hook::ViewListener
   # * :project - the current project
   def view_projects_form(context={})
     selected = context[:project].default_assignee.id unless context[:project].default_assignee.blank?
-    selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? # FIX also make sure system-wide default is an assignable user
-    selected ||= []
+    selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? or 
+										      not context[:project].assignable_users.member?(User.find(Setting.plugin_redmine_default_assign['default_assignee_id']).id)
+    selected ||= nil
     return content_tag(:p, context[:form].select(:default_assignee_id,  
                                       options_from_collection_for_select(context[:project].assignable_users, :id, :name, Integer(selected)), {:include_blank => :none}))
  
