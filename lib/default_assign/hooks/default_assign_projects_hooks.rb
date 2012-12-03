3bfc39c4af1052c4bcaa6936944e0ebad6f42899
Paul Gideon Dann <pdgiddie@gmail.com>
2012-12-03 10:37:37 +0000

    Don't show default assignee drop-down if there are no assignable members

--- lib/default_assign/hooks/default_assign_projects_hooks.rb
+++ lib/default_assign/hooks/default_assign_projects_hooks.rb
@@ -4,6 +4,7 @@ class DefaultAssignProjectHook < Redmine::Hook::ViewListener
   # * :form - the form builder
   # * :project - the current project
   def view_projects_form(context={})
+    return  if context[:project].assignable_users.empty?
     selected = context[:project].default_assignee.id unless context[:project].default_assignee.blank?
     selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? or 
 										      not context[:project].assignable_users.member?(User.find(Setting.plugin_redmine_default_assign['default_assignee_id']).id)
