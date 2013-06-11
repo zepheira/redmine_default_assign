b8ddf9069c93ba8a763a3bc5b9c1fa73cbb78204
Paul Gideon Dann <pdgiddie@gmail.com>
2013-06-11 15:04:31 +0100

    Don't fall back to global default if the project specifies no default assignee
    Previously, the behaviour was to use the assignee specified in the configuration
    screen if the project had no default assignee set. I don't think this makes
    sense; you may want to be able to set a blank assignee in certain projects, and
    it's unlikely you'll have so many projects that setting an assignee on each one
    will be that difficult.

--- lib/default_assign/hooks/default_assign_issues_hooks.rb
+++ lib/default_assign/hooks/default_assign_issues_hooks.rb
@@ -9,20 +9,10 @@ class DefaultAssignIssueHook < Redmine::Hook::ViewListener
     interactive_assignment = (interactive_assignment == 'true')
     return  if not interactive_assignment
 
-    selected = nil
     if not context[:project].default_assignee.blank?
-      selected = context[:project].default_assignee.id
-    else
-      default_default_id = Setting.plugin_redmine_default_assign['default_assignee_id']
-      if not default_default_id.blank?
-        default_default_assignee = User.find(default_default_id)
-        if context[:project].assignable_users.member?(default_default_assignee)
-          selected = default_default_assignee.id
-        end
-      end
+      context[:issue].assigned_to_id = context[:project].default_assignee.id
     end
 
-    context[:issue].assigned_to_id = selected
     nil
   end  
 end
